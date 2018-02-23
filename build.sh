c9fe145d4acfb789d20a9c24127a92a0ed397ebf
Pavlos Polianidis <pavlos.polianidis@ipsoft.com>
2018-02-23 15:26:39 +0100

    Lazy to comment

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-CONTAINER=grafana_temp_build
+BUILD_CONTAINER=grafana_temp_build
+GRAFANA_CONTAINER=grafana-musl-docker
 BUILD_DIR=/grafana_temp/src/github.com/grafana/grafana/
 
 # Initiating build in a centos container
-docker run --name ${CONTAINER} -p 3000:3000 -dti docker.io/centos
-docker cp grafana_build_centos.sh ${CONTAINER}:/grafana_build_centos.sh 
-docker exec -it ${CONTAINER} /grafana_build_centos.sh
+docker run --name ${BUILD_CONTAINER} -p 3000:3000 -dti docker.io/centos
+docker cp grafana_build_centos.sh ${BUILD_CONTAINER}:/grafana_build_centos.sh 
+docker exec -it ${BUILD_CONTAINER} /grafana_build_centos.sh
 
 # Copy assets locally
-docker cp ${CONTAINER}:${BUILD_DIR}/assets .
+docker cp ${BUILD_CONTAINER}:${BUILD_DIR}/assets .
 
 # Build grafana image
-docker build . -t  grafana-musl-docker
+docker build . -t  ${GRAFANA_CONTAINER}
 
 # Cleanup
-docker rm -f ${CONTAINER}
+docker rm -f ${BUILD_CONTAINER}
 docker rmi centos
 rm -rf assets
+docker run -d -p 3000:3000 --name ${GRAFANA_CONTAINER}-`date +%s` ${GRAFANA_CONTAINER}
